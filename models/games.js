a92055afd0174518345879f46db77c5c46c4d56f
Dineshk90 <shiki_arc@hotmail.com>
2022-10-11 14:50:55 +0800

    bugfix

--- models/games.js
+++ models/games.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const gameSchema = new mongoose.Schema({
   name: { type: String, required: true },
   title: { type: String, required: true },
-
   desc: { type: String, required: true },
   myImg: {
     data: Buffer,
